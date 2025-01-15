8bc8a297296a607c8b3856a69cb64d76b507adcb
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-01-14 22:39:09 -0500

    initial settings

---
+++ src/test/java/com/project/SpringAngularCrud/SpringAngularCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.SpringAngularCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
